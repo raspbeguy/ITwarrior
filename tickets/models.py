4c2d4ffbd35b5e88afbeee66f74ec8fa004dee15
Guy Godfroy <guy.godfroy@probayes.com>
2019-06-20 16:47:49 +0200

    add stuff for tickets

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid as pyuuid
 
-# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+            ('pending', 'Pending'),
+            ('completed', 'Completed'),
+            ('deleted', 'Deleted'),
+            ]
+    description = models.TextField('Description')
+    uuid        = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    status      = models.CharField('Status', choices=STATUS_CHOICES, default='pending')
+    autor       = models.CharField('Auteur')
+    depends     = models.ManyToManyField("self",symmetrical=False)
+    entry       = models.DateTimeField('Créaton')
+    due         = models.DateTimeField('Échéance')
+    end         = models.DateTimeField('Fin')
+
