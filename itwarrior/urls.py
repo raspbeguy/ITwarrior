ff0df317de3760392de41aab9b12421ef112da82
Guy Godfroy <guy.godfroy@probayes.com>
2019-06-25 14:11:35 +0200

    fix models

--- itwarrior/urls.py
+++ itwarrior/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('tickets/', include('tickets.urls')),
+    path('task/', include('task.urls')),
     path('admin/', admin.site.urls),
 ]
