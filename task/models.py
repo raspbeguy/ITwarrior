92db1a049956b2b52b4e25651c78bcc17a5da49a
Guy Godfroy <guy.godfroy@probayes.com>
2019-06-25 15:54:50 +0200

    add whatever

--- task/models.py
+++ task/models.py
@@ -2,20 +2,34 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+class Profile(models.Model):
+    """This is a user wrapping class for later use."""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.get_username()
+
+
 class Project(models.Model):
-    name        = models.CharField('Projet', max_length=50)
-    shortname   = models.CharField('Identifiant', max_length=50)
-    description = models.TextField('Description')
-    parent      = models.ForeignKey('self', verbose_name="Parent", blank=True)
+    name        = models.CharField('projet', max_length=50)
+    shortname   = models.CharField('identifiant', max_length=50)
+    description = models.TextField('description')
+    parent      = models.ForeignKey('self', verbose_name="parent", blank=True, null=True, on_delete=models.CASCADE)
+
+    def fully_qualified_name(self):
+        return self.shortname if self.parent is None else self.parent.fully_qualified_name()+'.'+self.shortname
+
+    def __str__(self):
+        return self.fully_qualified_name()
+
 
 class Tag(models.Model):
-    name        = models.CharField('Tag', max_length=50)
-    description = models.TextField('Description')
+    name        = models.CharField('tag', max_length=50)
+    description = models.TextField('description')
 
-class Uda(models.Model):
-    name        = models.CharField('Attribut', max_length=50)
-    description = models.TextField('Description')
-    value       = models.TextField('Valeur')
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     STATUS_CHOICES = [
@@ -25,29 +39,46 @@ class Task(models.Model):
             ('waiting', 'Waiting'),
             ('recurring', 'Recurring'),
             ]
-    description = models.TextField('Description', blank=False)
+    description = models.TextField('description', blank=False)
     uuid        = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, blank=False)
-    status      = models.CharField('Status', choices=STATUS_CHOICES, default='pending', max_length=9)
-    autor       = models.ForeignKey(User, verbose_name="Auteur", null=True, on_delete=models.SET_NULL, blank=True)
-    depends     = models.ManyToManyField("self", verbose_name="Dépendances", symmetrical=False, blank=True, related_name="task_depends")
-    entry       = models.DateTimeField('Date de créaton', blank=False)
-    due         = models.DateTimeField('Date d\'échéance', blank=True)
-    start       = models.DateTimeField('Date de début', blank=True)
+    status      = models.CharField('status', choices=STATUS_CHOICES, default='pending', max_length=9)
+    autor       = models.ForeignKey(Profile, verbose_name="auteur", null=True, on_delete=models.SET_NULL, blank=True)
+    depends     = models.ManyToManyField("self", verbose_name="dépendances", symmetrical=False, blank=True, related_name="task_depends")
+    entry       = models.DateTimeField('date de créaton', blank=False)
+    due         = models.DateTimeField('date d\'échéance', blank=True)
+    start       = models.DateTimeField('date de début', blank=True)
     end         = models.DateTimeField('date de fin', blank=True)
-    until       = models.DateTimeField('Date de fin de récurrence', blank=True)
-    scheduled   = models.DateTimeField('Date prévue', blank=True)
-    wait        = models.DateTimeField('Date d\'attente', blank=True)
-    modified    = models.DateTimeField('Date de modification', blank=False)
-    recur       = models.CharField('Périodicité', blank=True, max_length=50)
-    mask        = models.CharField('Masque', blank=True, max_length=256)
-    imask       = models.IntegerField('Indice de masque', blank=True)
-    parent      = models.ForeignKey('self', verbose_name="Parent", on_delete=models.CASCADE, blank=True, related_name="task_parent")
-    project     = models.ForeignKey(Project, verbose_name="Projet", on_delete=models.CASCADE, blank=True)
-    priority    = models.CharField('Priorité', blank=True, max_length=50)
-    tags        = models.ManyToManyField(Tag, verbose_name="Tags", blank=True)
+    until       = models.DateTimeField('date de fin de récurrence', blank=True)
+    scheduled   = models.DateTimeField('date prévue', blank=True)
+    wait        = models.DateTimeField('date d\'attente', blank=True)
+    modified    = models.DateTimeField('date de modification', blank=False)
+    recur       = models.CharField('périodicité', blank=True, max_length=50)
+    mask        = models.CharField('masque', blank=True, max_length=256)
+    imask       = models.IntegerField('indice de masque', blank=True)
+    parent      = models.ForeignKey('self', verbose_name="parent", on_delete=models.CASCADE, blank=True, related_name="task_parent")
+    project     = models.ForeignKey(Project, verbose_name="projet", on_delete=models.CASCADE, blank=True)
+    priority    = models.CharField('priorité', blank=True, max_length=50)
+    tags        = models.ManyToManyField(Tag, verbose_name="tags", blank=True)
 
 class Annotation(models.Model):
-    content     = models.TextField('Contenu')
-    author      = models.ForeignKey(User, verbose_name="Auteur", null=True, on_delete=models.SET_NULL)
-    date        = models.DateTimeField('Date')
-    task        = models.ForeignKey(Task, verbose_name="Tâche", on_delete=models.CASCADE)
+    content     = models.TextField('contenu')
+    author      = models.ForeignKey(Profile, verbose_name="auteur", null=True, on_delete=models.SET_NULL)
+    date        = models.DateTimeField('date')
+    task        = models.ForeignKey(Task, verbose_name="tâche", on_delete=models.CASCADE)
+
+
+class UdaDef(models.Model):
+    name        = models.CharField('attribut', max_length=50)
+    description = models.TextField('description')
+
+    def __str__(self):
+        return self.name
+
+
+class UdaVal(models.Model):
+    value       = models.TextField('valeur')
+    definition  = models.ForeignKey(UdaDef, verbose_name="définition", on_delete=models.PROTECT)
+    task        = models.ForeignKey(Task, verbose_name="tâche", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.task+":"+self.definition
